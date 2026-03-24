74a5a5af00f0d10b7ac257ddfaaa653f39e1db4e
PyRusAdmin <zh.vitaliy92@yandex.ru>
2026-03-24 15:04:19 +0300

    Формирование проекта

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+from dotenv import load_dotenv
+from loguru import logger
+
+load_dotenv()
+
+# Токен бота можно получить с помощью https://t.me/BotFather
+TOKEN: str = os.getenv("BOT_TOKEN")
+logger.info(f"Bot token: {TOKEN}")
