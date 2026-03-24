7c88d08f9492a396b4c06dfefefa3af1bfc4d592
PyRusAdmin <zh.vitaliy92@yandex.ru>
2026-03-24 17:30:28 +0300

    Добавлена запись в базу данных пользователя, который запустил бота и который отправил номер телефона через клавиатуру.

--- main.py
+++ main.py
@@ -3,16 +3,24 @@ import asyncio
 import logging
 import sys
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F, Router
+
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+from loguru import logger
 
 from config import TOKEN
 from keyboards.keyboards import contact_keyboard
+from services.database import create_tables, write_to_db_person
+
+router = Router(name=__name__)
 
 dp = Dispatcher()
+dp.include_router(router)
+
+logger.add("log/log.log")
 
 greet_message = (
     "Добро пожаловать в The Black 169 🖤\n\n"
@@ -30,13 +38,50 @@ async def command_start_handler(message: Message) -> None:
     """
     Этот обработчик получает сообщения с командой '/start'
     """
+    id_telegram = message.from_user.id
+    name_telegram = message.from_user.first_name
+    first_name_telegram = message.from_user.last_name
+    username_telegram = message.from_user.username
+    phone_telegram = ""
+
+    write_to_db_person(
+        id_telegram=id_telegram,
+        last_name_telegram=name_telegram,
+        first_name_telegram=first_name_telegram,
+        username_telegram=username_telegram,
+        phone_telegram=phone_telegram
+    )
+
     await message.answer(
         text=greet_message,
         reply_markup=contact_keyboard()
     )
 
 
+@router.message(F.contact)
+async def message_handler(message: Message) -> None:
+    """
+    Обработчик сообщений
+    """
+    id_telegram = message.from_user.id
+    name_telegram = message.from_user.first_name
+    first_name_telegram = message.from_user.last_name
+    username_telegram = message.from_user.username
+    phone_telegram = message.contact.phone_number
+    logger.info(f"Пользователь отправил контакт: {phone_telegram}")
+
+    write_to_db_person(
+        id_telegram=id_telegram,
+        last_name_telegram=name_telegram,
+        first_name_telegram=first_name_telegram,
+        username_telegram=username_telegram,
+        phone_telegram=phone_telegram
+    )
+
+
 async def main() -> None:
+    create_tables()  # Создание таблицы в базе данных
+
     # Initialize Bot Instance with default properties that will be passed to all API calls
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
--- services/database.py
+++ services/database.py
@@ -1,1 +1,46 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
+from peewee import *  # https://docs.peewee-orm.com/en/latest/index.html
+from loguru import logger
+
+db = SqliteDatabase("database.db")
+
+
+class Person(Model):
+    """База данных для хранения данных о пользователях Telegram"""
+
+    id_telegram = IntegerField(unique=True)  # id пользователя в Telegram
+    last_name_telegram = CharField(null=True)  # фамилия пользователя в Telegram
+    first_name_telegram = CharField(null=True)  # имя пользователя в Telegram
+    username_telegram = CharField(null=True)  # username пользователя в Telegram
+    phone_telegram = CharField(null=True)  # номер телефона пользователя в Telegram
+    updated_at = DateTimeField(default=datetime.now)  # дата и время обновления
+
+    class Meta:
+        database = db  # база данных
+        table_name = "person"  # название таблицы
+
+
+def create_tables():
+    """Создание таблицы в базе данных"""
+    db.create_tables([Person])
+
+
+def write_to_db_person(id_telegram, last_name_telegram, first_name_telegram, username_telegram, phone_telegram):
+    """Запись данных о пользователе в базу данных"""
+    try:
+        person, created = Person.get_or_create(id_telegram=id_telegram, defaults={
+            "last_name_telegram": last_name_telegram,
+            "first_name_telegram": first_name_telegram,
+            "username_telegram": username_telegram,
+            "phone_telegram": phone_telegram,
+        })
+        if not created:
+            person.last_name_telegram = last_name_telegram
+            person.first_name_telegram = first_name_telegram
+            person.username_telegram = username_telegram
+            person.phone_telegram = phone_telegram
+            person.updated_at = datetime.now()
+        person.save()
+    except Exception as e:
+        logger.exception(e)
