9102950a84c6030c570dd3690c8e68544f278278
PyRusAdmin <zh.vitaliy92@yandex.ru>
2026-03-24 16:48:14 +0300

    Добавлена клавиатура для отправки номера телефона.

---
+++ keyboards/keyboards.py
@@ -0,0 +1,9 @@
+from aiogram.types import KeyboardButton
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
+
+def contact_keyboard():
+    """Клавиатура для отправления номера телефона пользователя"""
+    builder = ReplyKeyboardBuilder()
+    builder.add(KeyboardButton(text="Отправить номер телефона", request_contact=True))
+    return builder.as_markup(resize_keyboard=True)
--- main.py
+++ main.py
@@ -9,45 +9,37 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message
 
 from config import TOKEN
-
-# All handlers should be attached to the Router (or Dispatcher)
+from keyboards.keyboards import contact_keyboard
 
 dp = Dispatcher()
+
+greet_message = (
+    "Добро пожаловать в The Black 169 🖤\n\n"
+    "В этом боте вы сможете:\n"
+    "🔹 проверять бонусы\n"
+    "🎁 узнать акции\n"
+    "🎀 получать подарки\n"
+    "📅 следить за мероприятиями\n\n"
+    "Отправьте свой номер телефона, чтобы начать 📱\n"
+)
 
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     """
-    This handler receives messages with `/start` command
+    Этот обработчик получает сообщения с командой '/start'
     """
-    # Most event objects have aliases for API methods that can be called in events' context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
-    # method automatically or call API method directly via
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
-    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
-
-
-@dp.message()
-async def echo_handler(message: Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
+    await message.answer(
+        text=greet_message,
+        reply_markup=contact_keyboard()
+    )
 
 
 async def main() -> None:
-    # Initialize Bot instance with default bot properties which will be passed to all API calls
+    # Initialize Bot Instance with default properties that will be passed to all API calls
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
-    # And the run events dispatching
+    # И распределение событий на забегах
     await dp.start_polling(bot)
 
 
